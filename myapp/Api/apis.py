cc3577648f7e0ec3c102ac2f25119047a4c288d2
jara <cuentapersonaluni@gmail.com>
2023-12-13 04:49:12 +0100

    cambios fallidos

--- myapp/Api/apis.py
+++ myapp/Api/apis.py
@@ -5,10 +5,14 @@ from rest_framework.response import Response
 from django.shortcuts import redirect
 from rest_framework import viewsets
 from .serializers import UserGet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import JWTAuthentication
 
 class UserData (viewsets.ModelViewSet):
+	authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args,**kwargs):
+		usuarios = User.object.all()
+		serializers = UserGet(usuarios, many=True)
+		return Response(serializers.data, status = status.HTTP_200_OK)
 
-    def get(self, request):
-    	user = request.user
-    	serializer = UserGet(user)
-    	return Response(serializer.data)
