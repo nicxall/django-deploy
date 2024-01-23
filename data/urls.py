157016847faeca5375c6f6cb02f7750944f9bd7c
juan <cuentapersonaluni@gmail.com>
2024-01-23 05:15:36 +0100

    se corregio error

--- data/urls.py
+++ data/urls.py
@@ -3,7 +3,7 @@ from .views import CreateTask, AuthenticationViewFactory
 
 urlpatterns = [
     path('create/task/', CreateTask.as_view(), name = 'createtaskapi'),
-    path('/', AuthenticationViewFactory.create_view('signin'), name = 'factorysignin'),
+    path('', AuthenticationViewFactory.create_view('signin'), name = 'factorysignin'),
     path('account/signup/', AuthenticationViewFactory.create_view('signup'), name = 'factorysignup'),
     path('account/logout/', AuthenticationViewFactory.create_view('logout'), name = 'factorylogout'),
 
